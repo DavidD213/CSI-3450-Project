383c5bab897de14850b36927b2437096aca3d1f9
yariqhasan <hasanyariq@gmail.com>
2021-12-10 20:00:38 -0500

    First initialization

--- login.js
+++ login.js
@@ -1,6 +1,8 @@
 const mysql = require("mysql");
 const express = require("express");
 const bodyParser = require("body-parser");
+const { DEC8_BIN } = require("mysql/lib/protocol/constants/charsets");
+const { redirect } = require("express/lib/response");
 const encoder = bodyParser.urlencoded();
 
 
@@ -38,11 +40,11 @@ app.get("",(req,res) => {
     res.render('log_In');
 });
 
-app.post("/",encoder, function(req,res){
+app.post("/log",encoder, function(req,res){
     var username = req.body.username;
     var password = req.body.password;
 
-    connection.query("select * from mydb.user where username = ? and password = ?",[username,password],function(error,results,fields){
+    connection.query("select * from mydb.admin where username = ? and password = ?",[username,password],function(error,results,fields){
         if (results.length > 0) {
             res.redirect("/Player");
         } else {
@@ -50,7 +52,55 @@ app.post("/",encoder, function(req,res){
         }
         res.end();
     })
-})
+});
+
+app.post("/Player",encoder, function(req,res){
+    var PLAYER_ID = req.body.PLAYER_ID;
+    var PLAYER_FNAME = req.body.PLAYER_FNAME;
+    var PLAYER_LNAME = req.body.PLAYER_LNAME;
+    var PLAYER_POSITION = req.body.PLAYER_POSITION;
+    var PLAYER_YEAR = req.body.PLAYER_YEAR;
+    var TEAM_ID = req.body.TEAM_ID;
+    
+    var sql = 'insert into mydb.player(PLAYER_ID, PLAYER_FNAME, PLAYER_LNAME, PLAYER_POSITION, PLAYER_YEAR, TEAM_ID) Values ?';
+    var values = [[PLAYER_ID,PLAYER_FNAME,PLAYER_LNAME,PLAYER_POSITION,PLAYER_YEAR,TEAM_ID]];
+    connection.query(sql, [values], function (err, result) {
+        if (err) throw err;
+        console.log("Number of records inserted: " + result.affectedRows);
+        res.redirect("/Player");
+   });
+
+});
+
+
+app.post("/team",encoder, function(req,res){
+    var TEAM_ID = req.body.TEAM_ID;
+    var TEAM_STATE = req.body.TEAM_STATE;
+    var TEAM_NAME = req.body.TEAM_NAME;
+    var TEAM_CONFERENCE = req.body.TEAM_CONFERENCE;
+    var TEAMSTATS_ID = req.body.TEAMSTATS_ID;
+    
+
+    
+    var sql = 'insert into mydb.team(TEAM_ID, TEAM_STATE, TEAM_NAME, TEAM_CONFERENCE, TEAMSTATS_ID) Values ?';
+    var values = [[TEAM_ID,TEAM_STATE,TEAM_NAME,TEAM_CONFERENCE,TEAMSTATS_ID]];
+    connection.query(sql, [values], function (err, result) {
+        if (err) throw err;
+        console.log("Number of records inserted: " + result.affectedRows);
+        res.redirect("/team");
+   });
+   
+});
+
+        
+
+
+
+
+
+
+
+
 
 // when login is success
 app.get("/Player",(req,res) => {
@@ -62,6 +112,19 @@ app.get("/team_status",(req,res) => {
     res.render('team_status');
 });
 
+app.get("/team",(req,res) => {
+    res.render('team');
+});
+app.get("/CRUD_Player",(req,res) => {
+    res.render('CRUD_Player');
+});
+
+app.get("/CRUD_Team",(req,res) => {
+    res.render('CRUD_Team');
+});
+app.get("/CRUD_Team_Status",(req,res) => {
+    res.render('CRUD_Team_Status');
+});
 
 // set app port 
 app.listen(4500);
--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,20 @@
   
     /*==================================================================
     [ Validate ]*/
+
+
+    function ClearFields() {
+        document.getElementById("form-group").value = "";
+   }
+   function dontGo(event) {
+    event.preventDefault();
+
+    }
+    document.getElementById("clickMe").addEventListener("click",dontGo);
+
+
+
+
     var input = $('.validate-input .input100');
 
     $('.validate-form').on('submit',function(){
@@ -30,18 +44,6 @@
         });
     });
 
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
 
     function showValidate(input) {
         var thisAlert = $(input).parent();
